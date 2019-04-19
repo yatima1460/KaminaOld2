a0daf65b263853cd1bc36cd87a1ab6483e2457be
Federico Santamorena <federicosantamorena@gmail.com>
2019-04-19 02:11:36 +0200

    more boilerplate

--- kamina.py
+++ kamina.py
@@ -1,6 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
+
+app: Flask = Flask(__name__)
+
 
 @app.route('/')
-def hello_world():
+def homepage() -> str:
     return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
